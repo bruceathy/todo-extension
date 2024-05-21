ed7afa1f7c05170034adc937c7d482f4eb8bec8f
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-05-21 11:02:44 -0400

    started to do functionality

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const toDoInput = document.getElementById("todo-input");
 const addBtn = document.querySelector("add-btn");
 const toDoList = document.getElementById("todo-list");
+
+addBtn.addEventListener("click", function () {
+  //   const toDo = toDoInput.value;
+  //   toDoInput.value = "";
+  //   toDoList.innerHTML += `<li>${toDo}</li>`;
+  console.log("tesing");
+});
