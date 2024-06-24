5a97639e0e0950c8d872cdc24f861dcedd45e866
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-06-24 10:39:04 -0400

    delete / edit / complete btns

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # To-Do List Chrome Extension
 
 - saves to do list items to localstorage
+- edit / delete list items after appending them to list
--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const toDoInput = document.getElementById("todo-input");
-const addBtn = document.querySelector("add-btn");
+const addBtn = document.getElementById("add-btn");
 const toDoList = document.getElementById("todo-list");
 
-addBtn.addEventListener("click", function () {
-  //   const toDo = toDoInput.value;
-  //   toDoInput.value = "";
-  //   toDoList.innerHTML += `<li>${toDo}</li>`;
-  console.log("tesing");
+function editToDo() {
+  const toDoItem = document.getElementById("todo-item");
+  toDoItem.contentEditable = true;
+  toDoItem.focus();
+}
+
+function deleteToDo(e) {
+  e.parentElement.remove();
+}
+
+// FIX THIS FUNCTION
+function completeToDo() {
+  const toDoItem = document.getElementById("todo-item");
+  toDoItem.style.textDecoration = "line-through";
+}
+
+addBtn.addEventListener("click", () => {
+  const toDo = toDoInput.value;
+  toDoInput.value = "";
+  toDoList.innerHTML += `<li><p id="todo-item">${toDo}</p> <button onclick="completeToDo(this)"><i class="fas fa-check"></i></button> <button onclick="editToDo(this)"><i class="fas fa-edit"></i></button> <button onclick="deleteToDo(this)"><i class="fas fa-trash"></i></button></li>`;
 });
