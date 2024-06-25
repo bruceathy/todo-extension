a8c6bce89e7f3e0cd5de098f87f07ad0b7707cb5
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-06-25 09:59:46 -0400

    update

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ const toDoInput = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const toDoList = document.getElementById("todo-list");
 
-function editToDo() {
-  const toDoItem = document.getElementById("todo-item");
-  toDoItem.contentEditable = true;
-  toDoItem.focus();
+function editToDo(e) {
+  e.contentEditable = true;
+  // e.focus();
 }
 
 function deleteToDo(e) {
   e.parentElement.remove();
 }
 
-// FIX THIS FUNCTION
-function completeToDo() {
-  const toDoItem = document.getElementById("todo-item");
-  toDoItem.style.textDecoration = "line-through";
-}
-
 addBtn.addEventListener("click", () => {
   const toDo = toDoInput.value;
-  toDoInput.value = "";
-  toDoList.innerHTML += `<li><p id="todo-item">${toDo}</p> <button onclick="completeToDo(this)"><i class="fas fa-check"></i></button> <button onclick="editToDo(this)"><i class="fas fa-edit"></i></button> <button onclick="deleteToDo(this)"><i class="fas fa-trash"></i></button></li>`;
+
+  if (toDo === "") {
+    alert("Please enter a task");
+    return;
+  } else {
+    toDoInput.value = "";
+    toDoList.innerHTML += `<li><button onclick="editToDo(this)">${toDo}</button> <button onclick="deleteToDo(this)"><i class="fas fa-trash"></i></button></li>`;
+  }
 });
