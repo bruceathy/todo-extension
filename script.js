05899ef76bf733228bd35bfd2f927af9603f79c8
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-06-29 02:16:09 -0400

    reset

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 const toDoInput = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
-const toDoList = document.getElementById("todo-list");
+const listContainer = document.getElementById("list-container");
 
-function editToDo(e) {
-  e.contentEditable = true;
-  // e.focus();
+let toDoList = [];
+const toDosFromLocalStorage = JSON.parse(localStorage.getItem("toDos"));
+
+if (toDosFromLocalStorage) {
+  toDoList = toDosFromLocalStorage;
+  render(toDoList);
 }
 
-function deleteToDo(e) {
-  e.parentElement.remove();
+// RENDER SAVED TO-DOS FROM LOCALSTORAGE
+function render(toDoList) {
+  let listItems = "";
+  toDos.forEach((toDo) => {
+    listItems += `
+    <li>
+      <span>${toDo}</span>
+      <button onclick="deleteToDo(this)">
+      <i class="fas fa-trash"></i>
+      </button>
+    </li>`;
+  });
+  toDoList.innerHTML = listItems;
 }
+
+// EDIT TO-DO
+// function editToDo(e) {
+//   e.contentEditable = true;
+//   e.focus();
+// }
+
+// DELETE TO-DO
+// function deleteToDo(e) {
+//   e.parentElement.remove();
+//   // localStorage.removeItem(`${e.previousElementSibling.textContent}`);
+//   // localStorage.clear();
+
+//   console.log(localStorage);
+// }
 
 addBtn.addEventListener("click", () => {
-  const toDo = toDoInput.value;
-
-  if (toDo === "") {
+  if (toDoInput.value === "") {
     return;
   } else {
+    toDoList.push(toDoInput.value);
+    localStorage.setItem("toDos", JSON.stringify(toDoList));
     toDoInput.value = "";
-    toDoList.innerHTML += `<li><button onclick="editToDo(this)">${toDo}</button> <button onclick="deleteToDo(this)"><i class="fas fa-trash"></i></button></li>`;
+    render(toDoList);
+
+    console.log(localStorage.getItem("toDos"));
   }
 });
