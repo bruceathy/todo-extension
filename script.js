06fb099a9e55e77cc7dbceae64b7a7fa0f8569ba
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-07-03 10:42:17 -0400

    brfoke

--- script.js
+++ script.js
@@ -2,28 +2,42 @@ const toDoInput = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const listContainer = document.getElementById("list-container");
 
-let toDoList = [];
-const toDosFromLocalStorage = JSON.parse(localStorage.getItem("toDos"));
+// let toDoList = [];
+// const toDosFromLocalStorage = JSON.parse(localStorage.getItem("toDos"));
 
-if (toDosFromLocalStorage.length > 0) {
-  toDoList = toDosFromLocalStorage;
-  render(toDoList);
-} else {
-  listContainer.innerHTML = "<p class='empty-msg'>Empty List</p>";
-}
+// if (toDosFromLocalStorage) {
+//   toDoList = toDosFromLocalStorage;
+//   render(toDoList);
+// } else {
+//   listContainer.innerHTML = "<p class='empty-msg'>Empty List</p>";
+// }
 
 addBtn.addEventListener("click", () => {
   if (toDoInput.value === "") {
     return;
   } else {
-    toDoList.push(toDoInput.value);
-    localStorage.setItem("toDos", JSON.stringify(toDoList));
+    // toDoList.push(toDoInput.value);
+    localStorage.setItem(`${toDoInput.value}`, JSON.stringify(toDoInput.value));
     toDoInput.value = "";
-    render(toDoList);
+    render(JSON.stringify(localStorage));
 
     console.log(localStorage.getItem("toDos"));
   }
 });
+
+// EDIT TO-DO
+function editToDo(e) {
+  e.contentEditable = true;
+  e.focus();
+}
+
+function deleteToDo(e) {
+  // toDoList.pop(e.parentElement.textContent);
+  localStorage.setItem("toDos", JSON.stringify(toDoList));
+  render(JSON.stringify(localStorage));
+
+  console.log(localStorage.getItem("toDos"));
+}
 
 function render(toDos) {
   let listItems = "";
@@ -35,20 +49,12 @@ function render(toDos) {
       <i class="fas fa-trash"></i>
       </button>
     </li>`;
+    localStorage.setItem(`${toDos[i]}`, JSON.stringify(toDos[i]));
+
+    console.log(localStorage.getItem(`${toDos[i]}`));
   }
-  listContainer.innerHTML = listItems;
-}
-
-// EDIT TO-DO
-function editToDo(e) {
-  e.contentEditable = true;
-  e.focus();
-}
-
-function deleteToDo(e) {
-  toDoList.pop(e.parentElement.textContent);
-  localStorage.setItem("toDos", JSON.stringify(toDoList));
-  render(toDoList);
-
-  console.log(localStorage.getItem("toDos"));
+  listContainer.innerHTML = localStorage;
+  if (toDos.length === 0) {
+    listContainer.innerHTML = "<p class='empty-msg'>Empty List</p>";
+  }
 }
