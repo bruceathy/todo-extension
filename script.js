87ed28b0c169390e2fc1db46d3ed06b81bf226f8
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-07-04 22:27:04 -0400

    up

--- script.js
+++ script.js
@@ -2,41 +2,33 @@ const toDoInput = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const listContainer = document.getElementById("list-container");
 
-// let toDoList = [];
-// const toDosFromLocalStorage = JSON.parse(localStorage.getItem("toDos"));
+let toDoList = [];
+const toDosFromLocalStorage = JSON.parse(localStorage.getItem("toDos"));
 
-// if (toDosFromLocalStorage) {
-//   toDoList = toDosFromLocalStorage;
-//   render(toDoList);
-// } else {
-//   listContainer.innerHTML = "<p class='empty-msg'>Empty List</p>";
-// }
+if (toDosFromLocalStorage) {
+  toDoList = toDosFromLocalStorage;
+  render(toDoList);
+} else {
+  listContainer.innerHTML = "<p class='empty-msg'>Empty List</p>";
+}
 
 addBtn.addEventListener("click", () => {
   if (toDoInput.value === "") {
     return;
   } else {
-    // toDoList.push(toDoInput.value);
-    localStorage.setItem(`${toDoInput.value}`, JSON.stringify(toDoInput.value));
+    toDoList.push(toDoInput.value);
     toDoInput.value = "";
-    render(JSON.stringify(localStorage));
+    render(toDoList);
 
     console.log(localStorage.getItem("toDos"));
   }
 });
 
-// EDIT TO-DO
-function editToDo(e) {
-  e.contentEditable = true;
-  e.focus();
-}
-
 function deleteToDo(e) {
-  // toDoList.pop(e.parentElement.textContent);
-  localStorage.setItem("toDos", JSON.stringify(toDoList));
-  render(JSON.stringify(localStorage));
-
+  toDoList.pop(`${e.parentElement.textContent}`);
   console.log(localStorage.getItem("toDos"));
+
+  render(toDoList);
 }
 
 function render(toDos) {
@@ -44,17 +36,20 @@ function render(toDos) {
   for (let i = 0; i < toDos.length; i++) {
     listItems += `
     <li>
-      <button onclick="editToDo(this)">${toDos[i]}</but>
+      <span>${toDos[i]}</span>
       <button onclick="deleteToDo(this)">
       <i class="fas fa-trash"></i>
       </button>
     </li>`;
-    localStorage.setItem(`${toDos[i]}`, JSON.stringify(toDos[i]));
 
-    console.log(localStorage.getItem(`${toDos[i]}`));
+    localStorage.setItem("toDos", JSON.stringify(toDos));
   }
-  listContainer.innerHTML = localStorage;
-  if (toDos.length === 0) {
+
+  if (toDoList.length === 0) {
     listContainer.innerHTML = "<p class='empty-msg'>Empty List</p>";
+  } else {
+    listContainer.innerHTML = listItems;
   }
 }
+
+console.log(localStorage.getItem("toDos"));
